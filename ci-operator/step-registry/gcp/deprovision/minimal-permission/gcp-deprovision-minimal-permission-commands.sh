3dc934567348b72d82c5e9ec758523a0e547d962
Jianli Wei <jiwei@redhat.com>
2025-02-27 10:45:56 +0800

    WIP: Enable minimal permission for GCP pre-submit jobs

--- ci-operator/step-registry/gcp/deprovision/minimal-permission/gcp-deprovision-minimal-permission-commands.sh
+++ ci-operator/step-registry/gcp/deprovision/minimal-permission/gcp-deprovision-minimal-permission-commands.sh
@@ -4,6 +4,10 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+if [ ! -f "${SHARED_DIR}/gcp_min_permissions.json" ]; then
+  echo "No 'gcp_min_permissions.json' found, aborted." && exit 0
+fi
+
 GOOGLE_PROJECT_ID="$(< ${CLUSTER_PROFILE_DIR}/openshift_gcp_project)"
 export GCP_SHARED_CREDENTIALS_FILE="${CLUSTER_PROFILE_DIR}/gce.json"
 sa_email=$(jq -r .client_email ${GCP_SHARED_CREDENTIALS_FILE})
